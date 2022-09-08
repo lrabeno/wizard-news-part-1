d6feb39852e5ee82340b62811b97aa1fc042b03f
Louis Rabeno <lrabeno@gmail.com>
2022-09-08 16:25:00 -0400

    added morgan

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+const morgan = require("morgan");
+app.use(morgan("dev"));
+
 app.get("/", (req, res) => res.send("Hello World!"));
 
 const PORT = 1337;
